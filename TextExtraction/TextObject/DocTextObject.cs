2b9e57d3bce83d65d576f704ce14cb079a65ca02
syonoki <syonoki@gmail.com>
2016-11-01 01:06:52 +0900

    text object setup

---
+++ TextExtraction/TextObject/DocTextObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextExtration.TextObject
+{
+    public class DocTextObject:ITextObject {
+
+
+        private string text_;
+        public string text { get; set; }
+    }
+}
