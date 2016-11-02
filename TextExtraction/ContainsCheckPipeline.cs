e48fb0dbdcc0954a4a5391f4d7bb01217a0c204d
syonoki <syonoki@gmail.com>
2016-11-03 00:09:06 +0900

    contains check pipeline added

---
+++ TextExtraction/ContainsCheckPipeline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TextExtration {
+    public class ContainsCheckPipeline {
+        private readonly IDictionary<ContainsCheckBlock, bool> containsCheckBlocksAndExpectedResultDirDictionary_;
+
+        public ContainsCheckPipeline(IDictionary<ContainsCheckBlock, bool> containsCheckBlocksAndExpectedResultDirDictionary) {
+            containsCheckBlocksAndExpectedResultDirDictionary_ = containsCheckBlocksAndExpectedResultDirDictionary;
+        }
+
+        public bool check(ITextObject text) {
+            foreach (var checkBlock in containsCheckBlocksAndExpectedResultDirDictionary_) {
+                if(checkBlock.Key.contains(text) != checkBlock.Value)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
